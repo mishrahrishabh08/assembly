c52ebceca014b7b940b1fd5de8bed8b5ed11ee44
mishrahrishabh08 <91203032+mishrahrishabh08@users.noreply.github.com>
2025-03-15 17:13:20 +0530

    assembler.sh

--- assembler.sh
+++ assembler.sh
@@ -6,5 +6,11 @@ filename="${1%%.*}" #remove the file extension
 as "${filename}.s" -o "${filename}.o"
 gcc -o "${filename}" "${filename}.o"
 
+#Using nasm and ld
+#nasm -f elf64 ${fileName}".s"
+#ld ${fileName}".o" -o ${fileName}
+
 # run the file if not $2 arguement , if -g provided open debugger
 [ "$2" == "-g" ] && gdb -q "${filename}" || "./${filename}"
+
+
